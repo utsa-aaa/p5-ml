7fa7270b951c169ceb8dd51242ad9665f2b9bf88
jordanrolj <jordanrj@umich.edu>
2024-04-11 20:59:35 -0400

    moved training func to inside class

--- main.cpp
+++ main.cpp
@@ -5,80 +5,84 @@
 #include "csvstream.hpp"
 #include <map>
 #include <set>
+#include <cmath>
 
 using namespace std;
 
 class Classifier{
-    public:
+    private:
     int num_posts;
     int vocab_size;
     map<string, int> word_freq;
     map<string, int> label_freq;
     map<std::pair<string, string>, int> c_with_w;
-    //one more - for each label c and word w, 
-    //the number of posts with label c that contain w
-    //idk how to do that one
 
-    
+
+
+
+    public:
+    void train(ifstream&fin){
+        std::vector<std::string> line;
+        while (read_csv_line(fin,line, ',')) {
+            num_posts ++;
+            string label = line[2];
+            string content = line[3];
+
+            //label not added yet
+            if (label_freq.find(label) == label_freq.end()){
+                label_freq[label] = 1;
+            }
+            else{
+                label_freq[label]++;
+            }
+
+
+            set<string> unique = unique_words(content);
+            for (auto word: unique){
+                if (word_freq.find(word) == word_freq.end()){
+                    //word not in classifier yet
+                    vocab_size++;
+                    word_freq[word] = 1;
+                }
+                else{
+                    word_freq[word]++;
+                }
+                //add to c_with_w
+                std::pair<string, string> label_word_pair = make_pair(label, word);
+                if (c_with_w.find(label_word_pair) == c_with_w.end()){
+                    c_with_w[label_word_pair] = 1;
+                }
+                else{
+                    c_with_w[label_word_pair]++;
+                }
+            
+            }
+
+        }
+
+    }
 };
  set<string> unique_words(const string &str) {
-        istringstream source(str);
-        set<string> words;
-        string word;
-        while (source >> word) {
-            words.insert(word);
-        }
-        return words;
+    istringstream source(str);
+    set<string> words;
+    string word;
+    while (source >> word) {
+        words.insert(word);
+    }
+    return words;
     }
 
-void train(Classifier &training_class, ifstream&fin){
-    std::vector<std::string> line;
-    while (read_csv_line(fin,line, ',')) {
-        training_class.num_posts ++;
-        string label = line[2];
-        string content = line[3];
 
-        //label not added yet
-        if (training_class.label_freq.find(label) == training_class.label_freq.end()){
-             training_class.label_freq[label] = 1;
-        }
-        else{
-            training_class.label_freq[label]++;
-        }
-
-
-        set<string> unique = unique_words(content);
-        for (auto word: unique){
-            if (training_class.word_freq.find(word) == training_class.word_freq.end()){
-                //word not in classifier yet
-                training_class.vocab_size++;
-                training_class.word_freq[word] = 1;
-            }
-            else{
-                 training_class.word_freq[word]++;
-            }
-            //add to c_with_w
-            std::pair<string, string> label_word_pair = make_pair(label, word);
-            if (training_class.c_with_w.find(label_word_pair) == training_class.c_with_w.end()){
-                 training_class.c_with_w[label_word_pair] = 1;
-            }
-            else{
-            training_class.c_with_w[label_word_pair]++;
-            }
-        
-        }
-
-    }
-
-}
 
 
 int main(int argc, char **argv){
     if(argc<3 || argc>4){
         cout << "Usage: main.exe TRAIN_FILE TEST_FILE [--debug]" << endl;
+        return 1;
     }
     if(argc==4 && strcmp(argv[3], "--debug")!=0){
         cout << "Usage: main.exe TRAIN_FILE TEST_FILE [--debug]" << endl;
+        return 1;
     }
 
     string filename1 = argv[1];
@@ -98,13 +102,19 @@ int main(int argc, char **argv){
     training_data.getheader();
     Classifier training;
 
-    //make function train
-    train(training, fin1);
+    
+    training.train(fin1);
 
 }
 
 
-    // tuirn each content to set
-    //adjust classifier
+
+
+
+
+
+
+
+
 
 
